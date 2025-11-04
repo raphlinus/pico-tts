e58e3a36386e48168ad1624b3b4bc5e3b480fd74
Raph Levien <raph.levien@gmail.com>
2025-11-04 09:42:48 -0800

    Tools for LPC phoneme speech synthesis
    At the moment, mostly experimenting with LPC.

---
+++ src/lpc.rs
@@ -0,0 +1,67 @@
+const ORDER: usize = 10;
+
+#[derive(Default)]
+pub struct Reflector {
+    ks: [f64; ORDER],
+    rms: f64,
+}
+
+fn get_correlation(buf: &[f64], lag: usize) -> f64 {
+    let mut sum = 0.0;
+    for i in 0..buf.len() - lag {
+        sum += buf[i] * buf[i + lag]
+    }
+    sum
+}
+
+fn get_correlations(buf: &[f64]) -> [f64; ORDER + 1] {
+    core::array::from_fn(|lag| get_correlation(buf, lag))
+}
+
+pub fn confidence(buf: &[f64], period: usize) -> f64 {
+    get_correlation(buf, period) / get_correlation(buf, 0)
+}
+
+impl Reflector {
+    pub fn new(buf: &[f64]) -> Self {
+        let coeffs = get_correlations(buf);
+        let mut result = Self::default();
+        result.translate_coeffs(&coeffs, buf.len());
+        result
+    }
+
+    fn translate_coeffs(&mut self, coeffs: &[f64], n_samples: usize) {
+        let mut b = [0.0; ORDER];
+        let mut d = [0.0; ORDER + 1];
+        self.ks[0] = -coeffs[1] / coeffs[0];
+        d[0] = coeffs[1];
+        d[1] = coeffs[0] + (self.ks[0] * coeffs[1]);
+
+        for i in 1..ORDER {
+            let mut y = coeffs[i + 1];
+            b[0] = y;
+            for j in 0..i {
+                b[j + 1] = d[j] + self.ks[j] * y;
+                y += self.ks[j] * d[j];
+                d[j] = b[j];
+            }
+            self.ks[i] = -y / d[i];
+            d[i + 1] = d[i] + self.ks[i] * y;
+            d[i] = b[i];
+        }
+        self.rms = d[ORDER] / n_samples as f64;
+    }
+
+    pub fn is_unvoiced(&self) -> bool {
+        const UNVOICED_THRESHOLD: f64 = 0.3;
+        self.ks[1] > UNVOICED_THRESHOLD
+    }
+
+    pub fn ks(&self) -> &[f64] {
+        &self.ks
+    }
+
+    pub fn rms(&self) -> f64 {
+        self.rms
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,88 @@
+use clap::Parser;
+
+mod lpc;
+
+#[derive(Parser, Debug)]
+enum Cmd {
+    Clip(Clip),
+    Lpc(Lpc),
+} 
+
+#[derive(Parser, Debug)]
+struct Clip {
+    audio_file: String,
+    clip_file: String,
+    #[arg(short, long)]
+    out_file: String,
+}
+
+#[derive(Parser, Debug)]
+struct Lpc {
+    audio_file: String,
+    start: f64,
+    end: f64,
+    #[arg(short, long)]
+    #[clap(default_value = "0.9375")]
+    preemph: f64,
+}
+
+fn read_wav(filename: String) -> (hound::WavSpec, Vec<i16>) {
+    let mut reader = hound::WavReader::open(&filename).expect("error opening input file");
+    let spec = reader.spec();
+
+    let samples = reader.samples().map(|x| x.unwrap()).collect::<Vec<i16>>();
+    (spec, samples)
+}
+
+fn main_clip(args: Clip) {
+    let (spec, samples) = read_wav(args.audio_file);
+    let mut writer = hound::WavWriter::create(args.out_file, spec).unwrap();
+    let clip = std::fs::read_to_string(&args.clip_file).expect("error reading clip file");
+    for line in clip.lines() {
+        let words = line.split_ascii_whitespace().collect::<Vec<_>>();
+        if words.len() >= 2 {
+            if let Ok(start) = words[1].parse::<f64>() {
+                if let Ok(end) = words[2].parse::<f64>() {
+                    let istart = (spec.sample_rate as f64 * start).round() as usize;
+                    let iend = (spec.sample_rate as f64 * end).round() as usize;
+                    for sample in &samples[istart..iend] {
+                        writer.write_sample(*sample).unwrap();
+                    }
+                }
+            }
+        }
+    }
+    writer.finalize().unwrap();
+
+}
+
+fn main_lpc(args: Lpc) {
+    let (spec, samples) = read_wav(args.audio_file);
+    let samples_f64 = samples.iter().map(|x| *x as f64).collect::<Vec<_>>();
+    let preemph = preemph(&samples_f64, args.preemph);
+    let istart = (spec.sample_rate as f64 * args.start).round() as usize;
+    let iend = (spec.sample_rate as f64 * args.end).round() as usize;
+    const FRAME_SIZE: usize = 400;
+    const WINDOW_SIZE: usize = 800;
+    let n_chunks = (iend - istart) / FRAME_SIZE - 1;
+    for i in 0..n_chunks {
+        let window = &preemph[istart + i * FRAME_SIZE..][..WINDOW_SIZE];
+        let coeffs = lpc::Reflector::new(&window);
+        println!("{:.3?}", coeffs.ks());
+    }
+}
+
+fn preemph(inp: &[f64], a: f64) -> Vec<f64> {
+    (0..inp.len())
+        .map(|i| inp[i] - inp.get(i.wrapping_sub(1)).cloned().unwrap_or_default() * a)
+        .collect()
+}
+
+fn main() {
+    let cmd = Cmd::parse();
+    println!("{cmd:?}");
+    match cmd {
+        Cmd::Clip(args) => main_clip(args),
+        Cmd::Lpc(lpc) => main_lpc(lpc),
+    }
+}
